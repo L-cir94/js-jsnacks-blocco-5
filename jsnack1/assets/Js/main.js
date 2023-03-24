c63cf9b2171b6cb97552df363d451b99b221f516
L-cir94 <luca.cirigliano94@hotmail.it>
2023-03-24 12:27:31 +0100

    done

---
+++ jsnack1/assets/Js/main.js
@@ -0,0 +1,61 @@
+
+/* consegna
+Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza. Calcola quanto pesano tutte le zucchine. 
+*/
+
+/* strumenti: 
+
+*/
+let zucchina = [
+    {
+        "varietà": "pipp",
+        "peso": "500",
+        "lunghezza": "19"
+    },
+    {
+        "varietà": "zucchina roood",
+        "peso": "300",
+        "lunghezza": "15"
+    },
+    {
+        "color": "zucchina rood",
+        "peso": "300",
+        "lunghezza": "15"
+    },
+        {
+        "varietà": "pipp",
+        "peso": "500",
+        "lunghezza": "19"
+    },
+    {
+        "varietà": "zucchina roood",
+        "peso": "300",
+        "lunghezza": "15"
+    },
+    {
+        "color": "zucchina saad",
+        "peso": "100",
+        "lunghezza": "10"
+    },
+        {
+        "varietà": "pipp",
+        "peso": "500",
+        "lunghezza": "19"
+    },
+    {
+        "varietà": "zucchina saad",
+        "peso": "100",
+        "lunghezza": "10"
+    },
+    {
+        "color": "zucchina saad",
+        "peso": "100",
+        "lunghezza": "10"
+    },
+        {
+        "varietà": "pipp",
+        "peso": "500",
+        "lunghezza": "19"
+    },
+
+]
