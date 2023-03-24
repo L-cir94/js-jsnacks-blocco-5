6852f1bc82cc1cf1f3fb8af746613cbefa6689e0
L-cir94 <luca.cirigliano94@hotmail.it>
2023-03-24 12:28:59 +0100

    sum with message

--- jsnack1/assets/Js/main.js
+++ jsnack1/assets/Js/main.js
@@ -9,53 +9,58 @@ Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna
 let zucchina = [
     {
         "varietà": "pipp",
-        "peso": "500",
-        "lunghezza": "19"
+        "peso": 500,
+        "lunghezza": 19
     },
     {
         "varietà": "zucchina roood",
-        "peso": "300",
-        "lunghezza": "15"
+        "peso": 300,
+        "lunghezza": 15
     },
     {
         "color": "zucchina rood",
-        "peso": "300",
-        "lunghezza": "15"
+        "peso": 300,
+        "lunghezza": 15
     },
         {
         "varietà": "pipp",
-        "peso": "500",
-        "lunghezza": "19"
+        "peso": 500,
+        "lunghezza": 19
     },
     {
         "varietà": "zucchina roood",
-        "peso": "300",
-        "lunghezza": "15"
+        "peso": 300,
+        "lunghezza": 15
     },
     {
         "color": "zucchina saad",
-        "peso": "100",
-        "lunghezza": "10"
+        "peso": 100,
+        "lunghezza": 10
     },
         {
         "varietà": "pipp",
-        "peso": "500",
-        "lunghezza": "19"
+        "peso": 500,
+        "lunghezza": 19
     },
     {
         "varietà": "zucchina saad",
-        "peso": "100",
-        "lunghezza": "10"
+        "peso": 100,
+        "lunghezza": 10
     },
     {
         "color": "zucchina saad",
-        "peso": "100",
-        "lunghezza": "10"
+        "peso": 100,
+        "lunghezza": 10
     },
         {
         "varietà": "pipp",
-        "peso": "500",
-        "lunghezza": "19"
+        "peso": 500,
+        "lunghezza": 19
     },
 
 ]
+let sommaLunghezza = 0
+let somma = 0;
+zucchina.forEach(zucchina => somma += zucchina.peso);
+zucchina.forEach(zucchina => sommaLunghezza += zucchina.lunghezza);
+console.log(`il peso complessivo delle zucchine è di ${somma}kg, la lunghezza totale è di ${sommaLunghezza}cm`);
